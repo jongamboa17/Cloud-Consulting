315305720288c3078240d1cd36614c67e9121cff
Jonathan Gamboa <jongamboa17@gmail.com>
2022-11-22 02:24:55 -0600

    tasks deleted

--- force-app/main/default/lwc/task/task.js
+++ force-app/main/default/lwc/task/task.js
@@ -1,39 +1,62 @@
 import { LightningElement, api} from 'lwc';
 import registerHours from '@salesforce/apex/TaskService.registerHours';
 import updateTaskState from '@salesforce/apex/TaskService.updateTaskState';
-import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+const SUCCESS_TITLE = 'Success';
+const SUCCESS_VARIANT = 'success';
+const INFO_VARIANT = 'info';
 export default class Task extends LightningElement {
     @api task;
     hours;
-    taskIsStarted=true;
-    
+    @api stateTask;
+
+    @api
+    get taskStarted(){
+        if (this.stateTask == 'In progress') {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
     handleInput(event) {
         this.hours = event.target.value;
-        console.log('HORAS' + this.hours);
-        console.log('ID' + this.task.Id);
     }
 
     handleLoadHours() {
         registerHours({taskId:this.task.Id, hoursToAdd:this.hours})     
         .then(result => {
             if(result==true){  
-                //let hoursSum;
-                  return this.updateTask();
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: '',
+                        message: this.hours + ' Hours Added To ' + this.task.Name,
+                        variant: INFO_VARIANT
+                    })
+                );
+                this.hours = null;
+                return this.updateTask();
             }
         })
         .catch(error=> console.log(JSON.stringify(error) + " Este es mi error")) 
     }
-    
+
     handleMarkCompleted() {
         let status = '';
         if (this.task.State__c==='In progress') {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: SUCCESS_TITLE,
+                    message: this.task.Name + ' Completed',
+                    variant: SUCCESS_VARIANT
+                })
+            );
             status='Completed';
         }
         if (this.task.State__c==='Not started yet') {
             status = 'In progress';
-        
+            this.taskIsStarted=true;
         }
-        
         
         updateTaskState({taskId:this.task.Id, state:status})
         .then(result => {
@@ -50,7 +73,4 @@ export default class Task extends LightningElement {
         const hoursAdded = new CustomEvent('hoursadded', {});
         this.dispatchEvent(hoursAdded);    
     }
-
-
-
 }
